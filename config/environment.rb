fdf7005d924e1f037df8e59a28cd4a50118c9443
Marc Capellupo <m_capellupo@hotmail.com>
2013-06-11 10:58:13 -0400

    first commit
    first

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+config.gem "jammit"
+# Initialize the rails application
+Server::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Server::Application.config.secret_token = '08e2e0a014a692aa874af8df5ac677ac0bff3f85b4f81338a43c803518a9841d16c748310b620a6c23a1327939959e9f3627839e4b292eb3588fc2452751e96f'
